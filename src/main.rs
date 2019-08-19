a15c913196fbe0af4461ed3086b2029dadd4c95e
Sönke Hahn <soenkehahn@gmail.com>
2019-08-19 18:03:48 -0400

    log incoming udp messages

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+use std::net::UdpSocket;
+
+fn main() -> Result <(), Box<std::error::Error>> {
+    let mut socket = UdpSocket::bind("0.0.0.0:1234")?;
+    let mut buf = [0; 1000];
+    loop {
+    let (amt, src) = socket.recv_from(&mut buf)?;
+    println!("amt: {}, src: {}", amt, src);
+    println!("{}", String::from_utf8_lossy(&buf));
+}
+    Ok(())
+}
