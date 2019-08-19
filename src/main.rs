670a70c75af3a1b9e7ad0424db4d0ec0f5ab9aa8
Sönke Hahn <soenkehahn@gmail.com>
2019-08-19 18:14:04 -0400

    add Justfile

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
+#![deny(warnings)]
+#![deny(clippy::all)]
+
 use std::net::UdpSocket;
 
-fn main() -> Result<(), Box<std::error::Error>> {
-    let mut socket = UdpSocket::bind("0.0.0.0:1234")?;
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let socket = UdpSocket::bind("0.0.0.0:1234")?;
     let mut buf = [0; 1000];
     loop {
         let (amt, src) = socket.recv_from(&mut buf)?;
         println!("amt: {}, src: {}", amt, src);
         println!("{}", String::from_utf8_lossy(&buf));
     }
-    Ok(())
 }
